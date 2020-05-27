b825f1604d83a6ac91df18f25342de698e8042d6
Steven Gutierrez <stematgut@gmail.com>
2020-05-27 12:22:12 -0700

    Set up basic react boilerplate, cleaned up files, and added title

---
+++ project1/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      {/* 
+      header
+      navbar
+      body
+      footer 
+      */}
+    </div>
+  );
+}
+
+export default App;
